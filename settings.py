118b08897973b2a805f9dae9997b0eb41fbcc7a1
Nicolas Couture <nicolas.coutue@gmail.com>
2015-12-26 06:18:36 +0000

    adding settings.py and wsgi.py for django 1.7.0

--- settings.py
+++ settings.py
@@ -1,128 +1,131 @@
-# Django settings for celerycrawler project.
+"""
+Django settings for celerycrawler project.
 
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'ua$=!zpmt&@_=paocb_d_hxg78kdmxu3%f$e&15_eecg%k-2uv'
+
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-TEMPLATE_DEBUG = DEBUG
 
-ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
-)
+TEMPLATE_DEBUG = True
 
-MANAGERS = ADMINS
+ALLOWED_HOSTS = []
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'sqlit3.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'America/Chicago'
+# Application definition
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
-
-SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
-USE_L10N = True
-
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'y!!%#sg$%_(%zdkyn9(efvb(7db!%gtxm58lt=*@!cgre=!(_h'
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'djcelery',
+    'celerycrawler'
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'celerycrawler.urls'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+WSGI_APPLICATION = 'celerycrawler.wsgi.application'
 
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'djcelery',
-    'crawler'
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
-)
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+
+########################
+# celery configuration #
+########################
 
 import djcelery
 djcelery.setup_loader()
-
 BROKER_BACKEND = "couchdb"
 BROKER_HOST = "localhost"
 BROKER_PORT = 5984
-BROKER_VHOST = "celery"
+BROKER_VHOST = "celerycrawler"
 
-CELERYD_CONCURRENCY = 2
-CELERY_QUEUES = {"retrieve": {"exchange": "default", "exchange_type": "direct", "routing_key": "retrieve"},
-                 "process": {"exchange": "default", "exchange_type": "direct", "routing_key": "process "},
-                 "celery": {"exchange": "default", "exchange_type": "direct", "routing_key": "celery"}}
+#BROKER_URL = 'amqp://guest:guest@localhost:5672'
 
-class MyRouter(object):
+CELERYD_CONCURRENCY = 5
+CELERY_ACCEPT_CONTENT = ['pickle', 'json', 'msgpack', 'yaml']
+CELERY_QUEUES = {"retrieve": {"exchange": "default",
+                              "exchange_type": "direct",
+                              "routing_key": "retrieve"},
+                 "process": {"exchange": "default",
+                             "exchange_type": "direct",
+                             "routing_key": "process "},
+                 "celery": {"exchange": "default",
+                            "exchange_type": "direct",
+                            "routing_key": "celery"}}
+
+
+class Router(object):
 
     def route_for_task(self, task, args=None, kwargs=None):
-        if task == "crawler.tasks.retrieve_page":
+        if task == "celerycrawler.tasks.retrieve_page":
             return { "queue": "retrieve" }
         else:
             return { "queue": "process" }
 
-CELERY_ROUTES = (MyRouter(), )
+CELERY_ROUTES = (Router(), )
 
 import couchdb
 
 server = couchdb.Server()
-db = server["celerycrawler"]
+try:
+    db = server.create("celerycrawler")
+except:
+    db = server["celerycrawler"]
 
-USER_AGENT = "CeleryCrawler; Your User Agent Here"
+USER_AGENT = "LowNICMangChuLa; AkPodGafeiPiDPil"
